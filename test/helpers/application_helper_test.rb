805a52261754db5d6c4c7f6c91ff563d2df9f621
jamcry <jamcry@hotmail.com>
2019-04-27 13:42:34 +0300

    Add test cases for Signup page

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -4,6 +4,7 @@ class ApplicationHelperTest < ActionView::TestCase
   @@base_title = "Ruby on Rails Tutorial Sample App"
   test "full title helper" do
     assert_equal full_title, @@base_title
-    assert_equal full_title("Help"), "Help | #{@@base_title}"
+    assert_equal full_title("Help"),    "Help | #{@@base_title}"
+    assert_equal full_title("Sign Up"), "Sign Up | #{@@base_title}"
   end
 end
