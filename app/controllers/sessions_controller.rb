8380e83637dceed13a89a6deb62132a183138913
jamcry <jamcry@hotmail.com>
2019-05-05 23:42:22 +0300

    Add login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in
+      log_in user
+      flash[:success] = "Successfully logged in!"
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email or password'
       render 'new'
