00c7066629c4099c093b3e722cb9e298cad81636
jamcry <jamcry@hotmail.com>
2019-05-05 23:31:41 +0300

    Update sessions controller actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in
+    else
+      flash[:danger] = 'Invalid email or password'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
