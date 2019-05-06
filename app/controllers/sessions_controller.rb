38f4a4217c0757e784adc0f9b4a934eb0ba6fac6
jamcry <jamcry@hotmail.com>
2019-05-07 00:09:41 +0300

    Add remember me option to the form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       flash[:success] = "Successfully logged in!"
       redirect_to user
     else
