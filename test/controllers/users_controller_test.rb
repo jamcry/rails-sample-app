c9e8704610a69d06e69351bb2d3ccf9b9fde7290
jamcry <jamcry@hotmail.com>
2019-05-08 16:26:17 +0300

    Add authorization for :edit and :update

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,40 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user   = users(:example)
+    @user_2 = users(:example2)
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
   end
 
+  test "should redirect edit if not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update if logged in" do
+    patch user_path(@user), params: { user: { name:  @user.name,
+                                              email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@user_2)
+    get edit_user_path(@user)
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@user_2)
+    patch user_path(@user), params: { user: { name:  @user.name,
+                                              email: @user.email } }
+    assert_redirected_to root_url
+  end
 end
