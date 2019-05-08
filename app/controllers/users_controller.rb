08437868862ca57efc80408eddd4b62709dedce6
jamcry <jamcry@hotmail.com>
2019-05-08 16:40:54 +0300

    Add friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,6 +47,7 @@ class UsersController < ApplicationController
       # Confirms a logged-in user
       def logged_in_user
         unless logged_in?
+          store_location
           flash[:danger] = "Please log in."
           redirect_to login_url
         end
@@ -55,6 +56,10 @@ class UsersController < ApplicationController
       # Confirms the correct user
       def correct_user
         @user = User.find(params[:id])
-        redirect_to(root_url) unless current_user?(@user)
+      
+        unless current_user?(@user)
+          flash[:warning] = "You are not authorized for this action."
+          redirect_to(root_url)
+        end
       end
 end
