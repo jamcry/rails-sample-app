c8ef17bf7c067497a5c5d1ae4f68e73d0c8ac2fd
jamcry <jamcry@hotmail.com>
2019-05-09 17:23:58 +0300

    Refactor with private #downcase_email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 	attr_accessor :remember_token, :activation_token
-	before_save { self.email.downcase! }
+	before_save 	:downcase_email
 	before_create :create_activation_digest
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :name, 	presence: true, length: { maximum: 50 }
@@ -38,10 +38,17 @@ class User < ApplicationRecord
 		return false if remember_digest.nil?
 		BCrypt::Password.new(remember_digest).is_password?(remember_token)
 	end
+	
+  private
 
-	# Creates and asssigns the activation token and digest
-	def create_activation_digest
-		self.activation_token  = User.new_token
-		self.activation_digest = User.digest(activation_token)
-	end
+			# Converts email to lower-case
+			def downcase_email
+				self.email = email.downcase
+			end
+			
+	    # Creates and asssigns the activation token and digest
+	    def create_activation_digest
+		    self.activation_token  = User.new_token
+		    self.activation_digest = User.digest(activation_token)
+	    end
 end
