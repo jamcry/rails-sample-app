d3abcd8889d92e555ae657e38013ef6330d5c9e2
jamcry <jamcry@hotmail.com>
2019-05-09 17:20:21 +0300

    Implement methods for activation token and digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-	attr_accessor :remember_token
+	attr_accessor :remember_token, :activation_token
 	before_save { self.email.downcase! }
+	before_create :create_activation_digest
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :name, 	presence: true, length: { maximum: 50 }
 	validates :email, presence: true, length: { maximum: 255 },
@@ -36,5 +37,11 @@ class User < ApplicationRecord
 	def authenticated?(remember_token)
 		return false if remember_digest.nil?
 		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	end
+
+	# Creates and asssigns the activation token and digest
+	def create_activation_digest
+		self.activation_token  = User.new_token
+		self.activation_digest = User.digest(activation_token)
 	end
 end
