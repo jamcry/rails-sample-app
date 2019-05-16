f428f114ac1070dbf290d769fded9117c8aaf443
jamcry <jamcry@hotmail.com>
2019-05-16 23:38:44 +0300

    Fix img_size bug

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,8 @@
 module UsersHelper
   #Returns the Gravatar for the given user.
-  def gravatar_for(user, options = { img_size: nil })
+  def gravatar_for(user, options = { size: nil })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{options[:img_size]}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{options[:size]}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
