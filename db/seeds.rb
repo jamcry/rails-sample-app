ed2eb9f460afa448edda5f46a10c693d8cf0f198
jamcry <jamcry@hotmail.com>
2019-05-24 13:47:38 +0300

    Add follow/unfollow functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
 # Users
+User.create!(name:"jamcry",email:"jamcry@hotmail.com",password:"foobar",password_confirmation:"foobar",
+    admin: true, activated: true, activated_at: Time.zone.now)
 User.create!(name:  "Example User",
   email: "example@railstutorial.org",
   password:              "foobar",
